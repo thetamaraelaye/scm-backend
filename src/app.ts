9de9072adc52294a6393f496ca9494bef119a124
biggaji <tobiajibade2017@yandex.com>
2024-03-19 08:04:47 +0100

    Codebase setup

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,11 @@
-import express, { Express } from "express"
-import mongoose from "mongoose"
-import cors from "cors"
+import express, { Express } from 'express';
+import cors from 'cors';
 
-const app: Express = express()
+const app: Express = express();
 
-const PORT: string | number = process.env.PORT || 4000
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
 
-app.use(cors())
-
-app.listen(PORT, () =>
-      console.log(`Server running on http://localhost:${PORT}`)
-    )
+const PORT: number = Number(process.env.PORT) || 4000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
