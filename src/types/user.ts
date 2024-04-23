385e4b9ee49fb4ac85b6d471d7306f8f810e9c28
Tamara <tamaraelaye.gmail.com>
2024-04-23 12:56:35 +0100

    feat(user): add additional fields on signup

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,6 +1,9 @@
+import { UserRole } from "../models/user.model";
+
 export interface UserSignup {
   first_name: string;
   last_name: string;
+  dial_code: string;
   phone_number: string;
   date_of_birth: string;
   email: string;
@@ -8,6 +11,7 @@ export interface UserSignup {
   confirm_password: string;
   account_name: string;
   bank_name: string;
+  role: UserRole;
   account_number: string;
   bvn: string;
 }
