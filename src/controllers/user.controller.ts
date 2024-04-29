057e557532a738621299855c09bc7fa6d95c80b9
Tamara <tamaraelaye.gmail.com>
2024-04-29 07:53:46 +0100

    feat(verify): add verify email controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,9 @@
 import { Request, Response, NextFunction } from 'express';
 import { UserService } from '../services/user.service';
-import { UserSignup, UserLogin } from '../types/user';
+import { UserSignup, UserLogin, verifyEmail } from '../types/user';
+import { jsonOne } from '../utils/generalResponse';
+import verifyEmailTemplate from '../templates/verifyEmail.template';
+import MailService from '../services/mail.service';
 
 const userService = new UserService(); // initialise a new instance of the user service
 
@@ -8,11 +11,12 @@ export const signup = async (req: Request, res: Response, next: NextFunction) =>
   try {
     const userData: UserSignup = req.body;
     const user = await userService.signup(userData);
+
     res.status(201).json({
       status: 201,
       success: true,
       user,
-      message: 'User created successfully',
+      message: 'User created successfully, Please check your email to verify your account',
     });
   } catch (error) {
     // console.error('Error signing up:', error);
@@ -36,5 +40,21 @@ export const login = async (req: Request, res: Response, next: NextFunction) => 
   } catch (error) {
     console.error('Error logging in:', error);
     res.status(401).json({ error: 'Invalid credentials' });
+  }
+};
+
+//verify email
+export const verifyUser = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const payload: verifyEmail = req.body;
+
+    await userService.verifyEmail(payload);
+    res.status(200).json({
+      status: 200,
+    });
+    //SENDING RESPONSE
+    return jsonOne<string>(res, 200, 'Email Verification Successfull.');
+  } catch (error) {
+    next(error);
   }
 };
