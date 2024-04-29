b705522d9974662d37a77ba2359d4ce69ce1131b
Tamara <tamaraelaye.gmail.com>
2024-04-29 07:53:05 +0100

    add root dir message on broswer

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import express, { Express } from 'express';
 import cors from 'cors';
 import startDB from './configs/db.config';
 import userRouter from './routes/user.routes';
+import path from 'path';
 
 startDB();
 
@@ -15,6 +16,12 @@ app.use(express.json()); //parse json requests first
 app.use(express.urlencoded({ extended: true })); //parse url encoded requests
 
 app.use(cors()); //enable cors after parsing request
+
+//on accessig the root directory via the browser
+app.get('/', (req, res) => {
+  // Send an HTML page with styled content
+  res.sendFile(path.join(__dirname, 'welcome'));
+});
 
 // Use user routes
 app.use('/api/v1/', userRouter);
