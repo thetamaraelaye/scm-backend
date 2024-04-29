fd931d275a8647d9245f407b10740a82114248a4
Tamara <tamaraelaye.gmail.com>
2024-04-29 08:08:42 +0100

    feat(verify): add verify email service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,9 +1,14 @@
 import { emailRegex, passwordRegex } from '../constants/db.constant';
 import { UserRepository } from '../repositories/user.repository'; // UserRepository from "../repositories/user.repository";
-import { UserLogin, UserSignup } from '../types/user.js';
+import { UserLogin, UserSignup, verifyEmail } from '../types/user.js';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import { BadRequestError } from '../helpers/handleError';
+import { User } from '../models/user.model';
+import { verifyOtp, generateOtp } from '../utils/otpGenerator';
+import Otp, { OtpType } from '../models/otp.model';
+import MailService from './mail.service';
+import verifyEmailTemplate from '../templates/verifyEmail.template';
 
 const userRepo = new UserRepository();
 
@@ -39,6 +44,29 @@ export class UserService {
         password: hashedPassword,
       });
       const { password: pwd, ...storeUserResult } = storeUserRecordResult.toObject();
+
+      //GENERATE OTP FOR MAIL VERIFICATION
+      let tokenExpiration: any = new Date();
+      tokenExpiration = tokenExpiration.setMinutes(tokenExpiration.getMinutes() + 10);
+
+      const otp: string = generateOtp(6);
+
+      let newOtp = new Otp({
+        userId: storeUserResult._id,
+        type: OtpType.VERIFICATION,
+        otp,
+        otpExpiration: new Date(tokenExpiration),
+      });
+      await newOtp.save();
+      //SEND VERIFICATION MAIL TO USER
+      const emailTemplate = verifyEmailTemplate(otp);
+      const mailService = MailService.getInstance();
+      await mailService.sendMail('', {
+        to: storeUserResult.email,
+        subject: 'Verify OTP',
+        html: emailTemplate.html,
+      });
+
       return storeUserResult;
     } catch (error) {
       throw error;
@@ -82,6 +110,87 @@ export class UserService {
       });
 
       return accessToken;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  //verify email
+  //   async verifyEmail(payload: verifyEmail) {
+  //     try {
+  //       const { email, otp } = payload;
+
+  //        //IF USER NOT FOUND
+  //       const userRecord = await userRepo.findByEmail(email);
+
+  //       if (userRecord) {
+  //         throw new BadRequestError('User already exist');
+  //       }
+
+  //      //VERIFYING OTP
+  //       if (!otp) {
+  //         throw new BadRequestError('OTP is required');
+  //       }
+  //        // If user is already verified
+
+  //       if (userRecord.is_verified) {
+  //         throw new BadRequestError('User email is already verified.');
+  //       }
+  //       //get user id by finding by ID
+  //       const User = await userRepo.findById(userRecord._id);
+  //     let isOtpValid = await verifyOtp(User._id, otp, OtpType.VERIFICATION);
+
+  //     if (!isOtpValid) {
+  //       throw new BadRequestError(
+  //         'This OTP has Invalid.')
+  //     }
+
+  //     userRepo.update(User._id, { is_verified: true });
+  //     user.is_verified = true;
+  //     User.save();
+  //     //DELETE OTP
+
+  //     userRepo.otpDelete(isOtpValid);
+
+  //   }
+  // }
+  async verifyEmail(payload: verifyEmail) {
+    try {
+      const { email, otp } = payload;
+
+      // Find user by email
+      const userRecord = await userRepo.findByEmail(email);
+
+      // If user not found
+      if (!userRecord) {
+        throw new BadRequestError('User not found');
+      }
+
+      // If user is already verified
+      if (userRecord.is_verified) {
+        throw new BadRequestError('User email is already verified.');
+      }
+
+      // VERIFYING OTP
+      if (!otp) {
+        throw new BadRequestError('OTP is required');
+      }
+
+      // Verify OTP
+      const isOtpValid = await verifyOtp(userRecord._id, otp, OtpType.VERIFICATION);
+
+      if (!isOtpValid) {
+        throw new BadRequestError('Invalid OTP');
+      }
+
+      // Update user's verification status
+      await userRepo.update(userRecord._id, { is_verified: true });
+
+      // Delete OTP
+      await userRepo.otpDelete(isOtpValid);
+
+      // Return success message or any other response as needed
+      return 'Email verified successfully';
     } catch (error) {
       throw error;
     }
