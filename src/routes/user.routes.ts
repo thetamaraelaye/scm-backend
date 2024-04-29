33b01b2f56cb2c4e1c2890268a1c29179ffa76f2
Tamara <tamaraelaye.gmail.com>
2024-04-29 08:06:16 +0100

    feat(verify): add route to verify email

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -3,13 +3,15 @@
 import { Router } from 'express';
 import * as userController from '../controllers/user.controller';
 
-export const userRouter = Router()
-
+export const userRouter = Router();
 
 // Route for user signup
 userRouter.post('/signup', userController.signup);
 
 // Route for user login
 userRouter.post('/login', userController.login);
+
+//Route for user verification
+userRouter.post('/verify-email', userController.verifyUser);
 
 export default userRouter;
