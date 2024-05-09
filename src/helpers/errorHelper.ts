592fe98b8381aafc8dda4d44aa00fcef10716683
Tamara <tamaraelaye.gmail.com>
2024-05-09 07:52:04 +0100

    create error handling enum

---
+++ src/helpers/errorHelper.ts
@@ -0,0 +1,31 @@
+/**
+ * @class ErrorHelper
+ * @description Intercerpts and re-formats the error object into a clean readable response for the client and abstract away any internal error details
+ */
+
+import { ErrorCodeName } from '../types/error';
+
+export default class ErrorHelper {
+  static ProcessError(error: any) {
+    switch (error.code) {
+      case 400:
+      case 401:
+      case 403:
+      case 404:
+        return {
+          success: false,
+          message: error.message,
+          code: error.code,
+          codeName: error.codeName,
+        };
+
+      default:
+        return {
+          success: false,
+          message: `An error occurred on our server while processing your request, try again later`,
+          code: 500,
+          codeName: ErrorCodeName.INTERNAL_SERVER_ERROR,
+        };
+    }
+  }
+}
